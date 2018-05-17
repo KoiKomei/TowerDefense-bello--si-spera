c33716b1f6c4d5d41af49a2c0a99d0d6532f826d
antyj1994 <antyj1994@gmail.com>
2018-05-17 20:52:13 +0200

    player nella scena principale

---
+++ TowerDefense/Assets/Scripts/FPSInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script /fps Input")]
+public class FPSInput : MonoBehaviour {
+
+    public float speed = 6.0f;
+    public float gravity = -9.8f;
+    private bool isGrounded;
+    public float jumpForce = 2.0f;
+    private CharacterController charController;
+
+	// Use this for initialization
+	void Start () {
+        charController = GetComponent<CharacterController>();
+        isGrounded = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded){
+            Jump();
+        }
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = gravity;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        charController.Move(movement);
+    }
+}
---
+++ TowerDefense/Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerCharacter : MonoBehaviour {
+
+    Rigidbody rb;
+    private bool isGrounded;
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (rb.velocity.y == 0 && transform.position.y < 2) isGrounded = true;
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+                Jump();
+        }
+    }
+
+    public void Jump(){
+        rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+        isGrounded = false;
+    }
+
+}
