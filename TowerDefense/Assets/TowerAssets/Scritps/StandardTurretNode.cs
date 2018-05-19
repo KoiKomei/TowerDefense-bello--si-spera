584f1d889572759de0815b30964d3e820cfbaa9a
luca2808 <32547791+luca2808@users.noreply.github.com>
2018-05-19 12:06:40 +0200

    wewe

---
+++ TowerDefense/Assets/TowerAssets/Scritps/StandardTurretNode.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class StandardTurretNode : MonoBehaviour {
+
+    public Color hoverColor;
+    public Vector3 positionOffset;
+    [HideInInspector]
+    public GameObject turret;
+    public Turret Standard;
+
+    private bool builded;
+    private Renderer rend;
+    private Color startColor;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+        builded = false;
+    }
+
+    void OnMouseDown()
+    {
+        if (!builded)
+        {
+            builded = true;
+            BuildTurret();
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+
+    public Vector3 GetBuildPosition()
+    {
+        return transform.position + positionOffset;
+    }
+
+    void BuildTurret()
+    {
+        GameObject _turret = (GameObject)Instantiate(Standard.prefab, GetBuildPosition(), Quaternion.identity);
+        turret = _turret;
+
+        Debug.Log("Turret build!");
+    }
+
+}
+
