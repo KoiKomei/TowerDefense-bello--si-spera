00af83b5d27342502b95148ce6ee7868a19a5b75
antyj1994 <antyj1994@gmail.com>
2018-05-24 01:07:43 +0200

    First GUI

--- TowerDefense/Assets/Scripts/SCRIPT DI ALEX/RelativeMovement.cs
+++ TowerDefense/Assets/Scripts/SCRIPT DI ALEX/RelativeMovement.cs
@@ -2,34 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 [RequireComponent(typeof(CharacterController))]
 
 public class RelativeMovement : MonoBehaviour {
 
-    private Animator animator;
     [SerializeField] private Transform target;
-    public float rotSpeed = 15f;
-    public float pushForce = 3.0f;
-    //private bool isshooting;
-   
-    public float moveSpeed = 6.0f;
-    public float sprint = 15.0f;
+    private Animator animator;
 
+    public float rotSpeed = 15.0f;
+    public float moveSpeed = 1.0f;
     public float jumpSpeed = 15.0f;
     public float gravity = -9.8f;
     public float terminalVelocity = -10.0f;
     public float minFall = -1.5f;
-    private float _vertSpeed;
-    private CharacterController _charController;
+    private float vertSpeed;
 
-    private ControllerColliderHit _contact;
-    // Use this for initialization
-    void Start () {
-        _charController = GetComponent<CharacterController>();
-        _vertSpeed = minFall;
+
+    private CharacterController characterController;
+    private ControllerColliderHit contact;
+
+	// Use this for initialization
+	void Start () {
+        vertSpeed = minFall;
+        characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
-        //isshooting = false;
     }
 	
 	// Update is called once per frame
@@ -40,85 +36,75 @@ public class RelativeMovement : MonoBehaviour {
 
         bool hitGround = false;
         RaycastHit hit;
-        if (_vertSpeed < 0 && Physics.Raycast(transform.position, Vector3.down, out hit))
+
+        if(vertSpeed<0 && Physics.Raycast(transform.position,Vector3.down,out hit))
         {
-            float check = (_charController.height + _charController.radius) / 1.9f;
+            float check = (characterController.height + characterController.radius) / 1.9f;
             hitGround = hit.distance <= check;
         }
-        
-        if (horInput != 0 || vertInput != 0) {
-            if (Input.GetKey("left shift"))
-            {
-                movement.x = horInput * sprint;
-                movement.z = vertInput * sprint;
-                movement = Vector3.ClampMagnitude(movement, sprint);
-                Quaternion tmp = target.rotation;
-                target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
-                movement = target.TransformDirection(movement);
-                target.rotation = tmp;
-                Quaternion direction = Quaternion.LookRotation(movement);
-                transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotSpeed * Time.deltaTime);
-               
-            }
-            else
-            {
-                movement.x = horInput * moveSpeed;
-                movement.z = vertInput * moveSpeed;
-                movement = Vector3.ClampMagnitude(movement, moveSpeed);
-                Quaternion tmp = target.rotation;
-                target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
-                movement = target.TransformDirection(movement);
-                target.rotation = tmp;
-                Quaternion direction = Quaternion.LookRotation(movement);
-                transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotSpeed * Time.deltaTime);
-               
-            }
+        if (Input.GetKeyDown("left shift"))
+        {
+            moveSpeed = 6.0f;
         }
+        if (Input.GetKeyUp("left shift"))
+        {
+            moveSpeed = 1.0f;
+        }
+        if (horInput != 0 || vertInput != 0)
+        {
+            movement.x = horInput*moveSpeed;
+            movement.z = vertInput * moveSpeed;
+            movement = Vector3.ClampMagnitude(movement, moveSpeed);
+            Quaternion tmp = target.rotation;
+            target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+            movement = target.TransformDirection(movement);
+            target.rotation = tmp;
+
+             Quaternion direction = Quaternion.LookRotation(movement);
+             transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotSpeed * Time.deltaTime);
+            //transform.rotation = Quaternion.LookRotation(movement);
+        }
+       
         animator.SetFloat("Speed", movement.magnitude);
         if (hitGround)
         {
-            if (Input.GetButtonDown("Jump"))
+            if (Input.GetButton("Jump"))
             {
-                _vertSpeed = jumpSpeed;
+                vertSpeed = jumpSpeed;
             }
             else
             {
-                _vertSpeed = minFall;
+                vertSpeed = minFall;
                 animator.SetBool("Jumping", false);
             }
         }
         else
         {
-            _vertSpeed += gravity * 5 * Time.deltaTime;
-            if (_vertSpeed < terminalVelocity)
-            {
-                _vertSpeed = terminalVelocity;
+            
+            vertSpeed += gravity * 5 * Time.deltaTime;
+            if (vertSpeed < terminalVelocity) { 
+                vertSpeed = terminalVelocity;
             }
             animator.SetBool("Jumping", true);
-            if (_charController.isGrounded)
+            if (characterController.isGrounded)
             {
-                if (Vector3.Dot(movement, _contact.normal) < 0)
+                if (Vector3.Dot(movement, contact.normal) < 0)
                 {
-                    movement = _contact.normal * moveSpeed;
+                    movement = contact.normal * moveSpeed;
                 }
                 else
                 {
-                    movement += _contact.normal * moveSpeed;
+                    movement += contact.normal * moveSpeed;
                 }
             }
         }
-        movement.y = _vertSpeed;
+        movement.y = vertSpeed;
         movement *= Time.deltaTime;
-        _charController.Move(movement);
+        characterController.Move(movement);
     }
 
-    void OnControllerColliderHit(ControllerColliderHit hit)
+    private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        Rigidbody body = hit.collider.attachedRigidbody;
-        if (body != null && !body.isKinematic) {
-            body.velocity = hit.moveDirection * pushForce;
-        }
-        _contact = hit;
+        contact = hit;
     }
-
 }
---
+++ TowerDefense/Assets/Scripts/SCRIPT DI ALEX/backup/OrbitCam.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCam : MonoBehaviour {
+
+    [SerializeField] private Transform target;
+    public float rotSpeed = 1.5f;
+    private float _roty;
+    private Vector3 _offset;
+
+	// Use this for initialization
+	void Start () {
+        _roty = transform.eulerAngles.y;
+        _offset = target.position - transform.position;
+	}
+
+    private void LateUpdate()
+    {
+        float horInput = Input.GetAxis("Horizontal");
+        if (horInput != 0)
+        {
+            _roty += horInput * rotSpeed;
+        }
+        else {
+            _roty += Input.GetAxis("Mouse X") * rotSpeed *3;
+        }
+        Quaternion rotation = Quaternion.Euler(0, _roty, 0);
+        transform.position = target.position - (rotation * _offset);
+        transform.LookAt(target);
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
