2389d99f783aa0bb683a767269f0d176f9aa2e4b
antyj1994 <antyj1994@gmail.com>
2018-05-25 18:07:21 +0200

    Aggiunto in-game menu, health bar interattiva, la GUI ora scala in base allo schermo

---
+++ TowerDefense/Assets/Scripts/Scripts di Antonio/UIController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+    [SerializeField] private SettingsPopup settingsPopup;
+    [SerializeField] private Slider healthBar;
+    [SerializeField] private Text healthLabel;
+
+    private bool isPopupOpen = false;
+
+	// Use this for initialization
+	void Start () {
+        LockCursor();
+        settingsPopup.Close();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthLabel.text = healthBar.value.ToString();
+        manageSettingsPopup();
+    }
+    
+    private void manageSettingsPopup() {
+        if (Input.GetButtonDown("Cancel") && !isPopupOpen)
+        {
+            isPopupOpen = true;
+            OnOpenSettings();
+            UnlockCursor();
+        }
+        else if (Input.GetButtonDown("Cancel") && isPopupOpen)
+        {
+            isPopupOpen = false;
+            OnCloseSettings();
+            LockCursor();
+        }
+    }
+
+    private void OnOpenSettings() {
+        settingsPopup.Open();
+    }
+
+    private void OnCloseSettings() {
+        settingsPopup.Close();
+    }
+
+    private void LockCursor() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void UnlockCursor() {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
+    public void RestartScene() {
+        SceneManager.LoadScene("TestAnt");
+    }
+
+    public void MainMenuScene() {
+        
+    }
+
+    public void ExitScene() {
+        SceneManager.LoadScene("prototipo");
+    }
+	
+	
+}
