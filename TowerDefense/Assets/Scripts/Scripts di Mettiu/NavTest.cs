7473db04d8b86665f9e342f82e788d56c459f6af
Mattiaman <32545256+Mattiaman@users.noreply.github.com>
2018-06-08 10:19:41 +0200

    inizio test

---
+++ TowerDefense/Assets/Scripts/Scripts di Mettiu/NavTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavTest : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform goal;
+
+	void Start()
+	{
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.destination = goal.position;
+	}
+}
