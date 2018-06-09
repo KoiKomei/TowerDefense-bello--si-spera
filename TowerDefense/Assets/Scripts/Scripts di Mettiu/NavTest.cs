10714ca837fbe9db29bc929b002389845053bee3
Mattiaman <32545256+Mattiaman@users.noreply.github.com>
2018-06-09 10:45:52 +0200

    navigazione con path di waypoints

--- TowerDefense/Assets/Scripts/Scripts di Mettiu/NavTest.cs
+++ TowerDefense/Assets/Scripts/Scripts di Mettiu/NavTest.cs
@@ -7,10 +7,43 @@ public class NavTest : MonoBehaviour {
 
 	// Use this for initialization
 	public Transform goal;
+	public float PlayerDetectionRadius;
+	public Vector3 dest;
+
+	NavMeshAgent agent;
 
 	void Start()
 	{
-		NavMeshAgent agent = GetComponent<NavMeshAgent>();
-		agent.destination = goal.position;
+		agent = GetComponent<NavMeshAgent>();
+
+		float randomOffsetX = Random.Range(-5f, 5f);
+		float randomOffsetZ = Random.Range(-5f, 5f);
+		dest = new Vector3(goal.position.x+randomOffsetX,goal.position.y,goal.position.z+randomOffsetZ);
+		agent.destination = dest;
+	}
+	 
+	void Update()
+	{
+		Collider[] colliders = Physics.OverlapSphere(transform.position, PlayerDetectionRadius);
+		//Debug.Log(colliders.Length);
+		bool found=false;
+		Collider player = null;
+		foreach (Collider c in colliders)
+		{
+			if (c.GetComponentInParent<CharacterController>() != null)
+			{
+				found = true;
+				player = c;
+				break;
+			}
+		}
+		if (found)
+		{
+			agent.destination = player.transform.position;
+		}
+		else
+		{
+			agent.destination = dest;
+		}
 	}
 }
---
+++ TowerDefense/Assets/Scripts/Scripts di Mettiu/Navigator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Assertions;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class Navigator : MonoBehaviour {
+
+	private NavMeshAgent agent;
+
+	[SerializeField] private Transform[] Waypoints;
+	[SerializeField] private bool loop;
+
+	private int goingTo;
+
+	// Use this for initialization
+	void Start () {
+		Assert.IsNotNull(Waypoints);
+		Assert.AreNotEqual(0, Waypoints.Length);
+
+		agent = GetComponent<NavMeshAgent>();
+		agent.autoBraking = false;
+
+		goingTo = -1;
+
+		GoToNext();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		if(!agent.pathPending && agent.remainingDistance < 0.5f)
+		{
+			GoToNext();
+		}
+
+	}
+
+	private void GoToNext()
+	{
+		if(!loop && goingTo == Waypoints.Length - 1)
+		{
+			agent.autoBraking = true;
+			return;
+		}
+		goingTo=(goingTo+1)%Waypoints.Length;
+		agent.destination = Waypoints[goingTo].position;
+	}
+}
