029b87aa0474dfb9eb869b28664ed34f94bdab48
KoiKomei <alex.parisella@gmail.com>
2018-06-09 21:24:23 +0200

    Nuovi modelli

--- TowerDefense/Assets/Scripts/SCRIPT DI ALEX/backup/IKController.cs
+++ TowerDefense/Assets/Scripts/SCRIPT DI ALEX/backup/IKController.cs
@@ -14,8 +14,7 @@ public class IKController : MonoBehaviour {
 
     public static bool ikActive = true;
     public Transform leftHandObj = null;
-    public GameObject obj1 = null;
-    public Transform leftHandObj2 = null;
+   
     public Transform lookObj = null;
 
     void Start()
