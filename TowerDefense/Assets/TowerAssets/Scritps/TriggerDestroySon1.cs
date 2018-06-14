92d6195a30d8032146252a000420945d9668e8ab
luca2808 <32547791+luca2808@users.noreply.github.com>
2018-06-14 18:24:31 +0200

    a

---
+++ TowerDefense/Assets/TowerAssets/Scritps/TriggerDestroySon1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerDestroySon1 : MonoBehaviour
+{
+    public TriggerDestroy trigger;
+    
+    private void OnTriggerEnter()
+    {
+        trigger.Destroy1();
+    }
+}
