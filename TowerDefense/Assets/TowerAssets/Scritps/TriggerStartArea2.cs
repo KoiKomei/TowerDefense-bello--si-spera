37bc30171f7bec736e5cde6f9535733fa88f5bc8
KoiKomei <alex.parisella@gmail.com>
2018-06-14 17:08:28 +0200

    REEEEEEEEE

---
+++ TowerDefense/Assets/TowerAssets/Scritps/TriggerStartArea2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerStartArea2 : MonoBehaviour {
+
+    [SerializeField] public Text runMessage;
+    public GameObject Collider;
+    public GameObject Portal1;
+
+    private float movementUp = 0.1f;
+    private bool levelStart = false;
+
+    private IEnumerator OnTriggerEnter()
+    {
+        runMessage.text = "          YOU HAVE TO CLEAN THE AREA";
+        yield return new WaitForSeconds(3);
+        runMessage.text = "";
+        levelStart = true;
+        Collider.GetComponent<BoxCollider>().enabled = true;
+
+    }
+
+    public void Update()
+    {
+        if (levelStart && Portal1.transform.position.y < 4.5)
+        {
+            Portal1.transform.Translate(0, movementUp, 0);
+        }
+    }
+
+}
