1ae1e86c84095e4910c8130c6f23b589390289a6
dsHitman <danielesalim@outlook.it>
2018-06-14 12:09:14 +0200

    gestione spawn nemici

---
+++ TowerDefense/Assets/Scripts/Script di Daniele/WanderingAI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAI : MonoBehaviour {
+
+    public float speed = 3.0f;
+    public float obstacleRange = 5.0f;
+
+    [SerializeField] private GameObject fireballPrefab;
+    private GameObject _fireball;
+
+    private bool _alive;
+
+	// Use this for initialization
+	void Start () {
+
+        _alive = true;
+	}
+
+    public void SetAlive(bool alive)
+    {
+        _alive = alive;
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+
+        if (_alive) {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+
+            if (Physics.SphereCast(ray, 0.75f, out hit))
+            {
+                GameObject hitObject = hit.transform.gameObject;
+
+                if (hitObject.GetComponent<PlayerCharacter>())
+                {
+                    if (_fireball == null)
+                    {
+                        _fireball = Instantiate(fireballPrefab) as GameObject;
+                        _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                        _fireball.transform.rotation = transform.rotation;
+                    }
+                }
+
+                else if (hit.distance < obstacleRange)
+                {
+                    float angle = Random.Range(-110, 110);
+                    transform.Rotate(0, angle, 0);
+                }
+            }
+
+        }
+    }
+}
